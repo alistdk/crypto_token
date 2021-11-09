a4873808a457c8205ad8cde2a920370ee128f0c7
Alistdk <alistdk@gmail.com>
2021-11-09 05:23:11 -0300

    push rut

--- crypto_app/views.py
+++ crypto_app/views.py
@@ -3,25 +3,25 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, "esse_app/index.html")
+    return render(request, "crypto_app/index.html")
 
 def blog(request):
-    return render(request, "esse_app/blog.html")
+    return render(request, "crypto_app/blog.html")
 
 def checkout(request):
-    return render(request, "esse_app/checkout.html")
+    return render(request, "crypto_app/checkout.html")
 
 def contact(request):
-    return render(request, "esse_app/contact.html")
+    return render(request, "crypto_app/contact.html")
 
 def regular_page(request):
-    return render(request, "esse_app/regular-page.html")
+    return render(request, "crypto_app/regular-page.html")
 
 def shop(request):
-    return render(request, "esse_app/shop.html")
+    return render(request, "crypto_app/shop.html")
 
 def single_b(request):
-    return render(request, "esse_app/single-blog.html")
+    return render(request, "crypto_app/single-blog.html")
 
 def single_p(request):
-    return render(request, "esse_app/single-product-details.html")
+    return render(request, "crypto_app/single-product-details.html")
